b88b56967c7460d2e5ef23345de3f6b300849701
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2024-03-24 13:29:47 +0530

    trycatch modified

--- 04_try_catch/script.js
+++ 04_try_catch/script.js
@@ -11,5 +11,26 @@ function addTwoNums(a, b) {
     console.log("Error!", err)
   }
 }
-addTwoNums("5", 5);
-console.log("It still works");
+//addTwoNums("5", 5);
+//console.log("It still works");
+
+
+
+function letterFinder(word, match) {
+  let condition1 = typeof (word) == 'string' && word.length >= 2
+  let condition2 = typeof (match) == 'string' && match.length === 1;
+
+  if (condition1 && condition2) {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        console.log('Found the', match, 'at', i)
+      } else {
+        console.log('---No match found at', i)
+      }
+    }
+  } else {
+    console.log("Please pass correct arguments to the function.");
+  }
+}
+
+//letterFinder("cat", "c");
