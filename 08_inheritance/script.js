ae44c427eeb9833f9335fcd00bdda21833ab0ba7
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2024-03-26 21:16:56 +0530

    New example added to Inheritance

--- 08_inheritance/script.js
+++ 08_inheritance/script.js
@@ -267,3 +267,51 @@ leo.makeSound(true); // purr, meow
 var cuddles = new Tiger();
 cuddles.makeSound(false); // Roar!
 cuddles.makeSound(true); // purr, Roar!
+
+// -- Example-4 --------------------------------
+
+// Task 1: Code a Person class
+class Person {
+  constructor(name = "Tom", age = 20, energy = 100) {
+    this.name = name
+    this.age = age
+    this.energy = energy
+  }
+  sleep() {
+    console.log(`I gained ${this.energy + 10} energy, cause I'm sleeping.`)
+  }
+  doSomethingFun() {
+    console.log(`I still have ${this.energy - 10} energy let's keep dancing!`)
+  }
+}
+
+// Task 2: Code a Worker class
+class Worker extends Person {
+  constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+    super(xp, hourlyWage);
+    this.name = name;
+    this.age = age;
+    this.energy = energy;
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+  goToWork() {
+    console.log(`{this.xp + 10} points`)
+  }
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+  const inter1 = new Worker('Bob', 21, 110, 0, 10)
+  inter1.goToWork()
+  return inter1
+}
+console.log(intern())
+
+// Task 4: Code a manager object, methods
+function manager() {
+  const manager1 = new Worker('Alice', 30, 120, 100, 30)
+  manager1.doSomethingFun()
+  return manager1
+}
+console.log(manager())
