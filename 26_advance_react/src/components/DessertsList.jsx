1649fd0c0dcdb78f72dafa3a03f77647a3ff055f
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-11 14:40:47 +0530

    Controlled vs uncontrolled component

---
+++ 26_advance_react/src/components/DessertsList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const DessertsList = (props) => {
+
+  const lowCaloriesDesserts = props.data
+    .filter((dessert) => {
+      return dessert.calories < 500;
+    })
+    .sort((a, b) => {
+      return a.calories - b.calories;
+    })
+    .map((dessert) => {
+      return (
+        <li>
+          {dessert.name} - {dessert.calories} cal
+        </li>
+      );
+    });
+  return (
+    <div>
+      <ul>{lowCaloriesDesserts}</ul>;
+    </div>
+  );
+};
+
+export default DessertsList;
